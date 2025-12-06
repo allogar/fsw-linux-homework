b8bad0b682dee1840e87e6280a44da8a0d76a59c
Alex <allogar@hotmail.com>
2025-12-06 20:04:59 +0100

    Add handler SIGINT to detect CTRL-C, and prepare mutexes to write/read values from different threads

--- client1.c
+++ client1.c
@@ -1,5 +1,6 @@
 // Compile: gcc -pthread -std=c11 -Wall -O2 client1.c -o client1
 // Execute: ./client1 0.0.0.0 4001 0.0.0.0 4002 0.0.0.0 4003
+#define _POSIX_C_SOURCE 200809L
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -9,13 +10,48 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <pthread.h>
+#include <signal.h>
 
+
+#define NUM_OF_THREADS  3
+
+typedef struct {
+    char last_value[4096];              // last value received from server TCP port
+    pthread_mutex_t lock;               // thread mutex: concurrent access protection to last_value
+} thread_state_t;
 
 typedef struct {
     char ip[64];
     int port;
     int id;
 } thread_args_t;
+
+// Global variables
+int g_sockets[NUM_OF_THREADS];              // one socket for each thread
+thread_state_t g_states[NUM_OF_THREADS];    // one state for each thread
+
+volatile sig_atomic_t g_stop_flag = 0;      // Stop signal
+
+// Handler SIGINT: To detect CTRL-C
+void handle_sigint(int sig) {
+    g_stop_flag = 1;
+
+    // Close all registered sockets (async-signal-safe)
+    for (int i = 0; i < NUM_OF_THREADS; i++) {
+        if (g_sockets[i] > 0) {
+            close(g_sockets[i]);
+            g_sockets[i] = -1;
+        }
+    }
+}
+
+void setup_sigint_handler() {
+    struct sigaction sa;
+    sa.sa_handler = handle_sigint;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sigaction(SIGINT, &sa, NULL);
+}
 
 void *connection_thread(void *arg) {
     thread_args_t *t = (thread_args_t *)arg;
@@ -26,6 +62,9 @@ void *connection_thread(void *arg) {
         return NULL;
     }
 
+    // Register socker globally
+    g_sockets[t->id] = sock;
+
     struct sockaddr_in addr;
     memset(&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
@@ -34,20 +73,31 @@ void *connection_thread(void *arg) {
     if (inet_pton(AF_INET, t->ip, &addr.sin_addr) <= 0) {
         fprintf(stderr, "[Thread %d] inet_pton failed: %s\n", t->id, strerror(errno));
         close(sock);
+        g_sockets[t->id] = -1;
         return NULL;
     }
 
     if (connect(sock, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
         fprintf(stderr, "[Thread %d] Error when connecting to socket: %s\n", t->id, strerror(errno));
         close(sock);
+        g_sockets[t->id] = -1;
         return NULL;
     }
 
     char buffer[4096];
     ssize_t n;
 
-    // Read from socket and write to stdout
-    while ((n = read(sock, buffer, sizeof(buffer))) > 0) {
+    // Received value update loop
+    while (!g_stop_flag && (n = read(sock, buffer, sizeof(buffer))) > 0) {
+        // Safety storage of last received value
+        pthread_mutex_lock(&g_states[t->id].lock);
+
+        size_t copy_len = (n < sizeof(g_states[t->id].last_value) - 1) ? n : sizeof(g_states[t->id].last_value) - 1;
+        memcpy(g_states[t->id].last_value, buffer, copy_len);
+        g_states[t->id].last_value[copy_len] = '\0';
+
+        pthread_mutex_unlock(&g_states[t->id].lock);
+
         if (write(STDOUT_FILENO, buffer, n) < 0) {
             fprintf(stderr, "[Thread %d] Error when writing to stdout: %s\n", t->id, strerror(errno));
             close(sock);
@@ -55,11 +105,15 @@ void *connection_thread(void *arg) {
         }
     }
 
-    if (n < 0) {
+     if (!g_stop_flag && n < 0) {
         fprintf(stderr, "[Thread %d] Error when reading from socket: %s\n", t->id, strerror(errno));
     }
 
     close(sock);
+    g_sockets[t->id] = -1;
+
+    printf("[Thread %d] Finishing...\n", t->id);
+
     return NULL;
 }
 
@@ -70,27 +124,47 @@ int main(int argc, char *argv[]) {
     }
 
     int nthreads = (argc - 1) / 2;
-    pthread_t *threads = malloc(sizeof(pthread_t) * nthreads);
-    thread_args_t *targs = malloc(sizeof(thread_args_t) * nthreads);
+    if (nthreads > NUM_OF_THREADS) {
+        fprintf(stderr, "Error: Max number of threads must be %d\n", NUM_OF_THREADS);
+        return 1;
+    }
+
+    setup_sigint_handler();
+
+    // Init sockets
+    for (int i = 0; i < NUM_OF_THREADS; i++) {
+        g_sockets[i] = -1;
+    }
+
+    // Init thread states
+    for (int i = 0; i < NUM_OF_THREADS; i++) {
+        pthread_mutex_init(&g_states[i].lock, NULL);
+        g_states[i].last_value[0] = '\0';
+    }
+
+    pthread_t threads[nthreads];
+    thread_args_t *targs[nthreads];
 
     for (int i = 0; i < nthreads; i++) {
-        strncpy(targs[i].ip, argv[1 + i*2], sizeof(targs[i].ip));
-        targs[i].port = atoi(argv[2 + i*2]);
-        targs[i].id = i;
+        targs[i] = malloc(sizeof(thread_args_t));
 
-        if (pthread_create(&threads[i], NULL, connection_thread, &targs[i]) != 0) {
+        snprintf(targs[i]->ip, sizeof(targs[i]->ip), "%s", argv[1 + i * 2]);
+        targs[i]->port = atoi(argv[2 + i * 2]);
+        targs[i]->id = i;
+
+        if (pthread_create(&threads[i], NULL, connection_thread, targs[i]) != 0) {
             fprintf(stderr, "Error creating thread %d\n", i);
             return 1;
         }
     }
 
-    // Wait for all threads to finish
+    // In case of CTRL-C: Wait for all threads to finish
     for (int i = 0; i < nthreads; i++) {
         pthread_join(threads[i], NULL);
+        free(targs[i]);
     }
 
-    free(threads);
-    free(targs);
+    printf("Process finished!.\n");
 
     return 0;
 }
