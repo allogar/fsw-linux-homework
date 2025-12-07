676400fcb826059656ec4a2573caef05b0d3765f
Alex <allogar@hotmail.com>
2025-12-07 14:10:25 +0100

    Final version of client1.c: format output JSON, use some defines, and remove unwanted logs.

--- client1.c
+++ client1.c
@@ -1,5 +1,3 @@
-// Compile: gcc -pthread -std=c11 -Wall -O2 client1.c -o client1
-// Execute: ./client1 0.0.0.0 4001 0.0.0.0 4002 0.0.0.0 4003
 #define _POSIX_C_SOURCE 200809L
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,18 +9,23 @@
 #include <sys/socket.h>
 #include <pthread.h>
 #include <signal.h>
-#include <time.h>  // for nanosleep
-
 
 #define NUM_OF_THREADS  3
 #define DEFAULT_VALUE   "--"
+#define MAX_VALUE_SIZE  32
+
+#define SERVER_IP_ADDR  "0.0.0.0"
+#define SERVER_TCP_PORT 4001
+
+#define PERIOD_MS       100
 
 // State of each client TCP thread
 typedef struct {
-    char last_value[4096];              // last value received from server TCP port
+    char last_value[MAX_VALUE_SIZE];    // last value received from server TCP port
     pthread_mutex_t lock;               // thread mutex: concurrent access protection to last_value
 } thread_state_t;
 
+// Arguments to create each client TCP thread
 typedef struct {
     char ip[64];
     int port;
@@ -57,38 +60,42 @@ void setup_sigint_handler() {
     sigaction(SIGINT, &sa, NULL);
 }
 
-void *connection_thread(void *arg) {
+// Client TCP thread function
+void *client_tcp_thread(void *arg) {
     thread_args_t *t = (thread_args_t *)arg;
 
+    // Create socket
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
-        fprintf(stderr, "[Thread %d] Error in socket: %s\n", t->id, strerror(errno));
+        //fprintf(stderr, "[Thread %d] Error in socket: %s\n", t->id, strerror(errno));
         return NULL;
     }
 
     // Register socker globally
     g_sockets[t->id] = sock;
 
+    // Config socket
     struct sockaddr_in addr;
     memset(&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
     addr.sin_port = htons(t->port);
 
     if (inet_pton(AF_INET, t->ip, &addr.sin_addr) <= 0) {
-        fprintf(stderr, "[Thread %d] inet_pton failed: %s\n", t->id, strerror(errno));
+        //fprintf(stderr, "[Thread %d] inet_pton failed: %s\n", t->id, strerror(errno));
         close(sock);
         g_sockets[t->id] = -1;
         return NULL;
     }
 
+    // Connect socket
     if (connect(sock, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
-        fprintf(stderr, "[Thread %d] Error when connecting to socket: %s\n", t->id, strerror(errno));
+        //fprintf(stderr, "[Thread %d] Error when connecting to socket: %s\n", t->id, strerror(errno));
         close(sock);
         g_sockets[t->id] = -1;
         return NULL;
     }
 
-    char buffer[4096];
+    char buffer[MAX_VALUE_SIZE];
     ssize_t n;
 
     // Received value update loop
@@ -101,24 +108,34 @@ void *connection_thread(void *arg) {
         memcpy(g_states[t->id].last_value, buffer, copy_len);
         g_states[t->id].last_value[copy_len] = '\0';
 
+        // Remove possible "end of line" chars
+        g_states[t->id].last_value[strcspn(g_states[t->id].last_value, "\r\n")] = '\0';
+
         pthread_mutex_unlock(&g_states[t->id].lock);
         pthread_mutex_unlock(&g_snapshot_lock);
     }
 
      if (!g_stop_flag && n < 0) {
-        fprintf(stderr, "[Thread %d] Error when reading from socket: %s\n", t->id, strerror(errno));
+        //fprintf(stderr, "[Thread %d] Error when reading from socket: %s\n", t->id, strerror(errno));
     }
 
+    // Close socket
     close(sock);
     g_sockets[t->id] = -1;
 
-    printf("[Thread %d] Finishing...\n", t->id);
+    //printf("[Thread %d] Finishing...\n", t->id);
 
     return NULL;
 }
 
-void main_loop_read_states(int num_ths) {
-    const long period_ns = 100 * 1000000L; // 1 ms = 1,000,000 ns
+void produce_json_output(long long timestamp_ms, char out[][MAX_VALUE_SIZE]) {
+    printf("{\"timestamp\": %lld, \"out1\": \"%s\", \"out2\": \"%s\", \"out3\": \"%s\"}\n", 
+        timestamp_ms, out[0], out[1], out[2]);
+    fflush(stdout);
+}
+
+void main_loop_output_values(int num_of_threads) {
+    const long period_ns = PERIOD_MS * 1000000L; // 1 ms = 1,000,000 ns
     struct timespec ts_next;
 
     // Using Fixed Absolute Schedule for accurate timing
@@ -128,18 +145,19 @@ void main_loop_read_states(int num_ths) {
     while (!g_stop_flag) {
         // Current loop timestamp (ms)
         long long ms_since_epoch = ts_next.tv_sec * 1000LL + ts_next.tv_nsec / 1000000LL;
-        printf("[Main] %lld ms\n", ms_since_epoch);
+        //printf("[Main] %lld ms\n", ms_since_epoch);
+
+        char values[num_of_threads][MAX_VALUE_SIZE];
 
         pthread_mutex_lock(&g_snapshot_lock); // Global lock
 
-        for (int id = 0; id < num_ths; id++) {
-            char value_copy[4096];
-
+        for (int id = 0; id < num_of_threads; id++) {
             pthread_mutex_lock(&g_states[id].lock); // Thread lock
             
             // Copy value
-            strncpy(value_copy, g_states[id].last_value, sizeof(value_copy));
-            value_copy[sizeof(value_copy)-1] = '\0';
+            size_t len = strnlen(g_states[id].last_value, MAX_VALUE_SIZE - 1);
+            memcpy(values[id], g_states[id].last_value, len);
+            values[id][len] = '\0';
             
             // Reset value
             strncpy(g_states[id].last_value, DEFAULT_VALUE, sizeof(g_states[id].last_value));
@@ -147,10 +165,13 @@ void main_loop_read_states(int num_ths) {
             
             pthread_mutex_unlock(&g_states[id].lock);
 
-            printf("[Main] Thread %d value: %s\n", id, value_copy);
+            //printf("[Main] Thread %d value: %s\n", id, values[id]);
         }
 
         pthread_mutex_unlock(&g_snapshot_lock);
+
+        // Generate JSON with recovered values, and print to stdout
+        produce_json_output(ms_since_epoch, values);
 
         // Next execution time
         ts_next.tv_nsec += period_ns;
@@ -166,57 +187,44 @@ void main_loop_read_states(int num_ths) {
 }
 
 int main(int argc, char *argv[]) {
-    if (argc < 3 || (argc % 2) == 0) {
-        fprintf(stderr, "Usage: %s <ip_address1> <port_no1> [<ip_address2> <port_no2> ...]\n", argv[0]);
-        return 1;
-    }
-
-    int nthreads = (argc - 1) / 2;
-    if (nthreads > NUM_OF_THREADS) {
-        fprintf(stderr, "Error: Max number of threads must be %d\n", NUM_OF_THREADS);
-        return 1;
-    }
+    const int nthreads = NUM_OF_THREADS;
 
     setup_sigint_handler();
 
-    // Init sockets
-    for (int i = 0; i < NUM_OF_THREADS; i++) {
+    for (int i = 0; i < nthreads; i++) {
+        // Init sockets
         g_sockets[i] = -1;
-    }
 
-    // Init thread states
-    for (int i = 0; i < NUM_OF_THREADS; i++) {
+        // Init thread states
         pthread_mutex_init(&g_states[i].lock, NULL);
         strncpy(g_states[i].last_value, DEFAULT_VALUE, sizeof(g_states[i].last_value));
         g_states[i].last_value[sizeof(g_states[i].last_value)-1] = '\0';
     }
 
+    // Create one thread for each server TCP port
     pthread_t threads[nthreads];
-    thread_args_t *targs[nthreads];
+    thread_args_t targs[nthreads];
 
     for (int i = 0; i < nthreads; i++) {
-        targs[i] = malloc(sizeof(thread_args_t));
+        snprintf(targs[i].ip, sizeof(targs[i].ip), "%s", SERVER_IP_ADDR);
+        targs[i].port = SERVER_TCP_PORT + i;
+        targs[i].id = i;
 
-        snprintf(targs[i]->ip, sizeof(targs[i]->ip), "%s", argv[1 + i * 2]);
-        targs[i]->port = atoi(argv[2 + i * 2]);
-        targs[i]->id = i;
-
-        if (pthread_create(&threads[i], NULL, connection_thread, targs[i]) != 0) {
-            fprintf(stderr, "Error creating thread %d\n", i);
+        if (pthread_create(&threads[i], NULL, client_tcp_thread, &targs[i]) != 0) {
+            //fprintf(stderr, "Error creating thread %d\n", i);
             return 1;
         }
     }
 
-    // Main loop: read variables every 100 ms
-    main_loop_read_states(nthreads);
+    // Main loop: Get values and write to stdout every 100 ms
+    main_loop_output_values(nthreads);
 
     // In case of CTRL-C: Wait for all threads to finish
     for (int i = 0; i < nthreads; i++) {
         pthread_join(threads[i], NULL);
-        free(targs[i]);
     }
 
-    printf("Process finished!.\n");
+    //printf("Process finished!.\n");
 
     return 0;
 }
